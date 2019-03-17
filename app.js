26b9cb6994f7c80171fb0b0058b9814f0cd6bbd8
Max Rochefort-Shugar <max.shugar@sony.com>
2019-03-17 22:07:05 +0000

    Can now move folders. Still buggy. Breadcrumbs not correct and duplicate folders being made.

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.post('/api/users', (req, res, err) => {
 				email: 'is required'
 			}
 		});
-	}
+	}  
 	
 	/* Validate password */
 	if(!req.body.password){
@@ -314,30 +314,120 @@ app.get('/api/users/:owner_id/resources/:resource_id', verify_token, function(re
 	
 });
 
+/* Format resources for tree view. */
+app.get('/api/users/:owner_id/treeview/:omit_id', verify_token, function(req,res) {
+	
+	var omit_id = req.params.omit_id;
+	
+	if(mongoose.Types.ObjectId.isValid(req.params.owner_id)){
+		var id = mongoose.Types.ObjectId(req.params.owner_id);
+		Users.find({_id: id}, function(err, user){
+			if(user.length === 0){
+				return res.json({error: "Owner with id " + req.params.owner_id + " doesn't exist."});
+			} else{
+				
+				var flat = [];
+				
+				
+				/* Generates ordered array of folders. */
+				function convert_to_flat(obj, parent_id){
+					
+					for(var i = 0; i < obj.items.length; i++){
+						if(obj.items[i].type == 'folder'){ /* No files just folders. */
+							flat.push( { text: obj.items[i].name, guid: obj.items[i].guid, parent_id: parent_id, children: [] } );
+							convert_to_flat(obj.items[i], obj.items[i].guid);
+						}
+					}
+					
+				}
+				
+				convert_to_flat(user[0].resources, 'root');
+				
+				var tree = [];
+				
+				/* Add root to tree */
+				for(var i = 0; i < flat.length; i++){
+					
+					if(flat[i].parent_id == 'root' && flat[i].guid != omit_id){
+						flat[i]["children"] = [];
+						tree.push(flat[i]);
+						flat.splice(i, 1);
+						i--;
+					}
+
+				}
+				
+				function add_to_tree(branch, folder){
+					
+					for(var i = 0; i < branch.length; i++){
+						if(branch[i].guid == folder.parent_id){
+							branch[i].children.push(folder);
+						}
+						else {
+							add_to_tree(branch[i].children, folder);
+						}
+					}
+				}
+				
+				/* Add each item in the tree. */
+				for(var i = 0; i < flat.length; i++){
+
+					if(flat[i].guid != omit_id)
+						add_to_tree(tree , flat[i]);
+					
+				}
+				
+				res.json(tree);
+				
+			}
+		});
+	} else {
+		return res.json({error: "user id '" + req.params.user_id + "' is invalid."});
+	}
+	
+});
+
 /* Create a new resource. 
 
 @param owner_id
 @param folder_id
 @param type
 @param revisions
-@param owner_id
 
 */
-app.post('/api/users/:user_id/resources', verify_token, (req, res) =>{
+app.post('/api/users/:owner_id/resources', verify_token, (req, res) =>{
 
-	var owner_id = req.params.user_id;
+	var owner_id = req.params.owner_id;
 	var folder_id = req.body.folder_id;
+	
 	var name = req.body.name;
 	var type = req.body.type;
+	var guid = req.body.guid;
+	var activity = req.body.activity;
+	var items = req.body.items;
+	var revisions = req.body.revisions;
+	var sharing = req.body.items;
 	
 	if(owner_id === undefined)
 		return res.json({ error: 'owner_id is required.' });
 	if(folder_id === undefined)
 		return res.json({ error: 'folder_id is required.' });
+	
 	if(name === undefined)
 		return res.json({ error: 'name is required.' });
 	if(type === undefined)
 		return res.json({ error: 'type is required' });
+	
+	if(guid === undefined)
+		guid = uuidv1();
+	if(activity === undefined)
+		activity = [];
+	if(items === undefined)
+		items = [];
+	if(revisions === undefined)
+		revisions = [];
+	if(type === undefined)
+		sharing = [];
 	
 	/* Check if owner_id is valid. */
 	if(mongoose.Types.ObjectId.isValid(owner_id)){
@@ -363,7 +453,7 @@ app.post('/api/users/:user_id/resources', verify_token, (req, res) =>{
 				}
 				
 				var folder = find_folder(user[0].resources);
-				console.log
+			
 				if(folder == null){
 					return res.json({error: "Resource with id '" + req.params.folder_id + "' doesn't exist."});
 				}
@@ -377,7 +467,7 @@ app.post('/api/users/:user_id/resources', verify_token, (req, res) =>{
 				
 				folder.items.push(
 					{
-						'guid': uuidv1(), 'name': name, 'type': type, 'items': [], 'revisions': [], 'sharing': [], 'activity': []
+						'guid': guid, 'name': name, 'type': type, 'items': items, 'revisions': revisions, 'sharing': sharing, 'activity': activity
 					}
 				);
 				
@@ -393,9 +483,9 @@ app.post('/api/users/:user_id/resources', verify_token, (req, res) =>{
 });
 
 /* Delete a resource. */
-app.delete('/api/users/:user_id/resources/:resource_id', verify_token, (req, res) =>{
+app.delete('/api/users/:owner_id/resources/:resource_id', verify_token, (req, res) =>{
 	
-	var owner_id = req.params.user_id;
+	var owner_id = req.params.owner_id;
 	var target_id = req.body.target_id;
 	var resource_id = req.params.resource_id;
 	
@@ -417,7 +507,8 @@ app.delete('/api/users/:user_id/resources/:resource_id', verify_token, (req, res
 					} else {
 						for(var i = 0; i < obj.items.length; i++){
 							if(obj.items[i].type == 'folder'){
-								res = find_folder(obj.items[i]);
+								var ret = find_folder(obj.items[i]);
+								if(ret != null) res = ret;
 							}
 						}
 					}
@@ -441,7 +532,66 @@ app.delete('/api/users/:user_id/resources/:resource_id', verify_token, (req, res
 						folder.items.splice(i, 1);
 				}
 				
-				console.log(folder);
+				const finalUser = new Users(user[0]);
+				return finalUser.save().then(()=> res.json(folder));
+					
+			}
+		});
+	} else {
+		return res.json({error: "owner_id '" + owner_id + "' is invalid."});
+	}
+	
+});
+
+/* Update a resource in a folder. */
+app.put('/api/users/:owner_id/resources/:resource_id', verify_token, (req, res) =>{
+	
+	var owner_id = req.params.owner_id;
+	var target = req.body.target;
+	var resource_id = req.params.resource_id;
+	
+	if(target === undefined)
+		return res.json({ error: 'target is required.' });
+
+	/* Check if owner_id is valid. */
+	if(mongoose.Types.ObjectId.isValid(owner_id)){
+		var id = mongoose.Types.ObjectId(owner_id);
+		Users.find({_id: id}, function(err, user){
+			if(user.length === 0){
+				return res.json({error: "owner_id " + owner_id + " not found."});
+			} else{
+					
+				function find_folder(obj){
+					var res = null;
+					if(obj.guid == resource_id){
+						res = obj;
+					} else {
+						for(var i = 0; i < obj.items.length; i++){
+							if(obj.items[i].type == 'folder'){
+								var ret = find_folder(obj.items[i]);
+								if(ret != null) res = ret;
+							}
+						}
+					}
+					return res;
+				}
+					
+				var folder = find_folder(user[0].resources);
+				if(folder == null){
+					return res.json({error: "Resource id '" + resource_id + "' is invalid."});
+				}
+				
+				/* Is user the owner of this resource? */
+				if(owner_id != req.client_id){
+					return res.json({error: "User with id " + req.client_id + " does not have access to this resource."});
+				}
+				
+				/* TODO - Check if user is authorized to access resource through sharing. */
+				
+				for(var i = 0; i < folder.items.length; i++){
+					if(folder.items[i].guid == target.guid)
+						folder.items[i] = target;
+				}
 				
 				const finalUser = new Users(user[0]);
 				return finalUser.save().then(()=> res.json(folder));
@@ -472,27 +622,6 @@ app.put('/api/resources/:id', (req, res) => {
 						else return res.json(resource);
 					});
 				}
-			}
-		});
-	} else {
-		return res.json({error: "id '" + req.params.id + "' is invalid."});
-	}
-	
-});
-
-/* Delete a resource. */
-app.delete('/api/resources/:id', (req, res) => {
-	
-	if(mongoose.Types.ObjectId.isValid(req.params.id)){
-		var id = mongoose.Types.ObjectId(req.params.id);
-		Resources.find({_id: id}, function(err, resource){
-			if(resource.length == 0){
-				return res.json({error: "Resource with id " + req.params.id + " not found."});
-			} else{
-				Resources.deleteOne({ _id: id }, function (err) {
-				  if (err) return res.json(err);
-				  else return res.json({success: 'Resource ' + id + ' deleted successfully.'});
-				});
 			}
 		});
 	} else {
